8b9c3dc9be999dac5526b71c2dfae19c9ff1cac8
Dmitrii Serdun <dmitriiserdun@gmail.com>
2018-11-23 19:49:30 +0300

    init

---
+++ Calc.py
@@ -0,0 +1,116 @@
+import numpy
+# scipy.special for the sigmoid function expit()
+import scipy.special
+# library for plotting arrays
+import matplotlib.pyplot
+
+
+# ensure the plots are inside this notebook, not an external window
+class NeuralNetwork:
+
+    # initialise the neural network
+    def __init__(self, inputnodes, hiddennodes, outputnodes, learningrate):
+        # set number of nodes in each input, hidden, output layer
+        self.inodes = inputnodes
+        self.hnodes = hiddennodes
+        self.onodes = outputnodes
+
+        # link weight matrices, wih and who
+        # weights inside the arrays are w_i_j, where link is from node i to node j in the next layer
+        # w11 w21
+        # w12 w22 etc
+        self.wih = numpy.random.normal(0.0, pow(self.inodes, -0.5), (self.hnodes, self.inodes))
+        self.who = numpy.random.normal(0.0, pow(self.hnodes, -0.5), (self.onodes, self.hnodes))
+
+        # learning rate
+        self.lr = learningrate
+
+        # activation function is the sigmoid function
+        self.activation_function = lambda x: scipy.special.expit(x)
+
+        pass
+
+    # train the neural network
+    def train(self, inputs_list, targets_list):
+        # convert inputs list to 2d array
+        inputs = numpy.array(inputs_list, ndmin=2).T
+        targets = numpy.array(targets_list, ndmin=2).T
+
+        # calculate signals into hidden layer
+        hidden_inputs = numpy.dot(self.wih, inputs)
+        # calculate the signals emerging from hidden layer
+        hidden_outputs = self.activation_function(hidden_inputs)
+
+        # calculate signals into final output layer
+        final_inputs = numpy.dot(self.who, hidden_outputs)
+        # calculate the signals emerging from final output layer
+        final_outputs = self.activation_function(final_inputs)
+
+        # output layer error is the (target - actual)
+        output_errors = targets - final_outputs
+        # hidden layer error is the output_errors, split by weights, recombined at hidden nodes
+        hidden_errors = numpy.dot(self.who.T, output_errors)
+
+        # update the weights for the links between the hidden and output layers
+        self.who += self.lr * numpy.dot((output_errors * final_outputs * (1.0 - final_outputs)),
+                                        numpy.transpose(hidden_outputs))
+
+        # update the weights for the links between the input and hidden layers
+        self.wih += self.lr * numpy.dot((hidden_errors * hidden_outputs * (1.0 - hidden_outputs)),
+                                        numpy.transpose(inputs))
+
+        pass
+
+    # query the neural network
+    def query(self, inputs_list):
+        # convert inputs list to 2d array
+        inputs = numpy.array(inputs_list, ndmin=2).T
+
+        # calculate signals into hidden layer
+        hidden_inputs = numpy.dot(self.wih, inputs)
+        # calculate the signals emerging from hidden layer
+        hidden_outputs = self.activation_function(hidden_inputs)
+
+        # calculate signals into final output layer
+        final_inputs = numpy.dot(self.who, hidden_outputs)
+        # calculate the signals emerging from final output layer
+        final_outputs = self.activation_function(final_inputs)
+
+        return final_outputs
+
+
+
+# all_values = data_list[0].split(',')
+# image_array = np.asfarray(all_values[1:]).reshape((28,28))
+# plt.imshow(image_array, cmap='Greys', interpolation='None')
+# scaled_input=np.asfarray(all_values[1:])/(255*0.9)+0.1
+# print(scaled_input)
+# plt.show()
+
+# scorecard = []
+#
+# # go through all the records in the test data set
+# for record in test_data_list:
+#     # split the record by the ',' commas
+#     all_values = record.split(',')
+#     # correct answer is first value
+#     correct_label = int(all_values[0])
+#     # scale and shift the inputs
+#     inputs = (numpy.asfarray(all_values[1:]) / 255.0 * 0.99) + 0.01
+#     # query the network
+#     outputs = n.query(inputs)
+#     # the index of the highest value corresponds to the label
+#     label = numpy.argmax(outputs)
+#     # append correct or incorrect to list
+#     if (label == correct_label):
+#         # network's answer matches correct answer, add 1 to scorecard
+#         scorecard.append(1)
+#     else:
+#         # network's answer doesn't match correct answer, add 0 to scorecard
+#         scorecard.append(0)
+#         pass
+#
+#     pass
+#
+# scorecard_array = numpy.asarray(scorecard)
+# print ("performance = ", scorecard_array.sum() / scorecard_array.size)
---
+++ LineInterpolation.py
@@ -0,0 +1,139 @@
+import json
+import random
+
+import matplotlib.pyplot as plt
+import numpy as np
+import numpy
+
+from Calc import NeuralNetwork
+
+sizeInput = 401
+input_nodes = sizeInput - 1
+hidden_nodes = 200
+output_nodes = 2
+learning_rate = 0.1
+formatedTrainData = []
+
+
+# Підготовка навчальних даних
+
+train_30_data_incorrect = json.loads(open("train_data/30_train_data_incorrect.json").read())['result']
+train_30_data_correct = json.loads(open("train_data/30_train_data_correct.json").read())['result']
+flush = numpy.zeros(sizeInput)
+firsPerson = train_30_data_correct
+secondPerson = train_30_data_incorrect
+
+
+
+# Підготовка тестових даних
+test_incorrect_data = json.loads(open("test_data/tests_incorect_data.json").read())['result'][0]
+test_correct_data = json.loads(open("test_data/tests_corect_data.json").read())['result'][0]
+
+
+
+def interpolateAndNormalizeDate(dots, identificator):
+    newXX = []
+    newYY = []
+    for i in range(1, len(dots['yReal']) - 1):
+
+        x1 = dots['xReal'][i]
+        y1 = dots['yReal'][i]
+        x2 = dots['xReal'][i + 1]
+        y2 = dots['xReal'][i + 1]
+        if x1 - x2 != 0:
+            k = (y1 - y2) / (x1 - x2)
+            b = y2 - k * x2
+            for j in range(x2 - x1): newXX.append(x2 + j)
+            for j in range(x2 - x1): newYY.append((x2 + j) * k + b)
+
+    tmp = []
+    maxValue = max(newYY)
+    minValue = min(newYY)
+    tmp.append(identificator)
+    for i in range(len(newYY)):
+        current = newYY[i]
+        numerator = current - minValue
+        denominator = maxValue - minValue
+        normValue = numerator / denominator
+        tmp.append(normValue)
+    new = []
+    for line in tmp[::5]: new.append(line)
+    return new
+
+
+def showUserTest():
+    plt.plot(train_30_data_incorrect[0]['xReal'], train_30_data_incorrect[0]['yReal'], lw=2)
+    plt.plot(train_30_data_correct[0]['xReal'], train_30_data_correct[0]['yReal'], lw=2)
+    plt.legend(('1 user real test incorrect', '2 user real test correct'))
+    plt.gca().invert_yaxis()
+
+
+def drawNormalizarionValue(normValue, msg):
+    for i in range(len(normValue)): plt.plot(normValue[i])
+    plt.legend(["graph of normalized data for the test: " + msg])
+    plt.show()
+
+
+def prepareArray(result):
+    flush = numpy.zeros(sizeInput)
+    for z in range(len(flush)):
+        if z < len(result[:sizeInput]): flush[z] = result[:sizeInput - 1][z]
+    return flush
+
+
+def guessTest(msg, json):
+    result = interpolateAndNormalizeDate(json, 1)
+    formatedTestData = prepareArray(result)
+    result = n.query(((numpy.asfarray(formatedTestData[:sizeInput - 1]) * 0.99) + 0.01))
+    drawNormalizarionValue([formatedTestData], msg)
+    print(str(msg) + " ->")
+    print(str(result))
+
+
+# Підготовка тренувальних даних
+
+
+
+def prepareDataFor1Test():
+    for x in range(len(firsPerson)):
+        result = interpolateAndNormalizeDate(firsPerson[x], 0)
+        formatedTrainData.append(prepareArray(result[:sizeInput]))
+    drawNormalizarionValue(formatedTrainData, "First test")
+
+
+def prepareDataFor2Test():
+    secondPersonData=[]
+    for x in range(len(secondPerson)):
+        result = interpolateAndNormalizeDate(firsPerson[x], 1)
+        flush = prepareArray(result[:sizeInput])
+        formatedTrainData.append(flush)
+        secondPersonData.append(flush)
+    drawNormalizarionValue(secondPersonData, "Second test")
+
+
+def trainNetwork():
+    epochs = 20
+    for e in range(epochs):
+        random.shuffle(formatedTrainData)
+        for record in formatedTrainData:
+            all_values = record
+
+
+            inputs = (numpy.asfarray(all_values[1:]) * 0.99) + 0.01
+            targets = numpy.zeros(output_nodes) + 0.01
+            targets[int(all_values[0])] = 0.99
+            n.train(inputs, targets)
+
+
+showUserTest()
+prepareDataFor2Test()
+
+prepareDataFor1Test()
+n = NeuralNetwork(input_nodes, hidden_nodes, output_nodes, learning_rate)
+trainNetwork()
+
+
+guessTest("(0) [result for correct test: ]: ", test_correct_data)
+guessTest("(1) [result for incorrect test: ]: ", test_incorrect_data)
+
+plt.show()
